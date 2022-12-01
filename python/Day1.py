c91e66b374bb814bb73849e2a16cc2ab6da438e0
Joshua Hunt <jhunt@scottlogic.com>
2022-12-01 16:30:56 +0000

    Removed unneeded string join

--- python/Day1.py
+++ python/Day1.py
@@ -1,5 +1,5 @@
 file = "aoc22/src/main/java/resources/Day1Input.txt"
 
-print(max([sum([int(l.strip()) for l in elf.split("\n")]) for elf in "".join(open(file).readlines()).split("\n\n")]))
+print(max([sum([int(l.strip()) for l in elf.split("\n")]) for elf in open(file).read().split("\n\n")]))
 
-print(sum(sorted([sum([int(l.strip()) for l in elf.split("\n")]) for elf in "".join(open(file).readlines()).split("\n\n")])[-3:]))
+print(sum(sorted([sum([int(l.strip()) for l in elf.split("\n")]) for elf in open(file).read().split("\n\n")])[-3:]))

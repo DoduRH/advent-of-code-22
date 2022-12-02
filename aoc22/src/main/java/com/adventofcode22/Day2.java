c38687dde940dd57fb6f8be7fdfb67da1c09bfaf
Joshua Hunt <jhunt@scottlogic.com>
2022-12-02 10:12:36 +0000

    Refined java solution

--- aoc22/src/main/java/com/adventofcode22/Day2.java
+++ aoc22/src/main/java/com/adventofcode22/Day2.java
@@ -4,14 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * AOC Day 1
  */
 public final class Day2 {
     public static void main(String[] args) {
+        System.out.println(solution("/Day2Input.txt"));
         System.out.println(solution2("/Day2Input.txt"));
     }
 
@@ -20,69 +19,28 @@ public final class Day2 {
         assert inputStream != null;
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
-        List<Integer> elves = new java.util.ArrayList<>(Collections.emptyList());
-
         Integer currentScore = 0;
         String line;
 
         try {
             while ((line = reader.readLine()) != null) {
-                // System.out.print(line);
-                line = line.replace("X", "R").replace("Y", "P").replace("Z", "S");
-                line = line.replace("A", "R").replace("B", "P").replace("C", "S");
+                line = line.replace("X", "0").replace("Y", "1").replace("Z", "2");
+                line = line.replace("A", "0").replace("B", "1").replace("C", "2");
 
                 String[] scores = line.split(" ");
 
-                Integer p1;
-                Integer p2;
-                switch (scores[0]) {
-                    case "R":
-                        p1 = 0;
-                        break;
-
-                    case "P":
-                        p1 = 1;
-                        break;
-
-                    case "S":
-                        p1 = 2;
-                        break;
-
-                    default:
-                        p1 = -1;
-                }
-
-                switch (scores[1]) {
-                    case "R":
-                        p2 = 0;
-                        break;
-
-                    case "P":
-                        p2 = 1;
-                        break;
-
-                    case "S":
-                        p2 = 2;
-                        break;
-
-                    default:
-                        p2 = -1;
-                }
+                Integer p1 = Integer.valueOf(scores[0]);
+                Integer p2 = Integer.valueOf(scores[1]);
 
                 if (p1 == p2) {
                     currentScore += 3;
-                    // System.out.println(" Draw");
                 } else if ((p1 - p2 + 3) % 3 == 1) {
                     currentScore += 0;
-                    // System.out.println(" P1 wins");
                 } else if ((p2 - p1 + 3) % 3 == 1) {
                     currentScore += 6;
-                    // System.out.println(" P2 wins");
                 }
 
                 currentScore += p2 + 1;
-
-                // System.out.println(currentScore);
             }
         } catch (
 
@@ -104,47 +62,14 @@ public final class Day2 {
 
         try {
             while ((line = reader.readLine()) != null) {
-                System.out.print(line);
-                line = line.replace("A", "R").replace("B", "P").replace("C", "S");
-                line = line.replace("X", "L").replace("Y", "D").replace("Z", "W");
+                line = line.replace("A", "0").replace("B", "1").replace("C", "2");
+                line = line.replace("X", "-1").replace("Y", "0").replace("Z", "1");
 
                 String[] scores = line.split(" ");
 
-                Integer p1;
-                Integer p2;
-                switch (scores[0]) {
-                    case "R":
-                        p1 = 0;
-                        break;
-
-                    case "P":
-                        p1 = 1;
-                        break;
-
-                    case "S":
-                        p1 = 2;
-                        break;
-
-                    default:
-                        p1 = -1;
-                }
-
-                switch (scores[1]) {
-                    case "W":
-                        p2 = (p1 + 1) % 3;
-                        break;
-
-                    case "L":
-                        p2 = (p1 - 1 + 3) % 3;
-                        break;
-
-                    case "D":
-                        p2 = p1;
-                        break;
-
-                    default:
-                        p2 = -1;
-                }
+                Integer p1 = Integer.valueOf(scores[0]);
+                Integer modifier = Integer.valueOf(scores[1]);
+                Integer p2 = (p1 + modifier + 3) % 3;
 
                 if (p1 == p2) {
                     currentScore += 3;

c080a92a03e794bea51d91e70ee54a5ff0ee85a5
Josh <yr18478@bristol.ac.uk>
2022-12-03 18:08:31 +0000

    Added python 1 liner

--- python/Day3.py
+++ python/Day3.py
@@ -18,14 +18,15 @@ for line in lines:
 
     for char in commonChars:
         total += ord(char)
-        if char == char.upper():
+        if char.isupper():
             total -= 65-27
         else:
             total -= 96
 
 print("Part 1")
-print(total)
-print(157)
+print("Actual   ", 157)
+print("Result   ", total)
+print("One liner", sum([sum([ord(char) - 65 + 27 if char.isupper() else ord(char) - 96 for char in set(line[:len(line) // 2]).intersection(line[len(line) // 2:])]) for line in lines]))
 
 total = 0
 # Part 2
@@ -37,11 +38,12 @@ for l1, l2, l3 in zip(lines[0::3], lines[1::3], lines[2::3]):
         if char == "\n":
             continue
         total += ord(char)
-        if char == char.upper():
+        if char.isupper():
             total -= 65-27
         else:
             total -= 96
 
 print("Part 2")
-print(total)
-print(70)
+print("Actual   ", 70)
+print("Result   ", total)
+print("One liner", sum([sum([0 if char == "\n" else ord(char) - 65 + 27 if char.isupper() else ord(char) - 96 for char in set(l1).intersection(l2).intersection(l3)]) for l1, l2, l3 in zip(lines[0::3], lines[1::3], lines[2::3])]))
